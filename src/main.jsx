b79ff112ef29a96fd4c73e4b5ccf09edf58fd710
pavly111 <pavly.nader1@gmail.com>
2026-07-06 22:19:33 +0300

    fix routing and vite config for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/conference-memes">
-  <App />
-</BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
